f92685804ae727dc9256b275dbc855e31d2d449d
gusmarks <32940640+gusmarks@users.noreply.github.com>
2021-02-03 17:04:13 -0800

    adding main files
    most of the files are here, however these files lack the wxwidget dependencies

---
+++ chiro-pro/chiro-pro/app.h
@@ -0,0 +1,13 @@
+#ifndef __BASE_H
+#define __BASE_H
+#include <wx/frame.h>
+#include <wx/textctrl.h>
+#include <wx/wx.h>
+class MainApplication: public wxApp
+{
+public: 
+	virtual bool OnInit();
+};
+
+
+#endif
